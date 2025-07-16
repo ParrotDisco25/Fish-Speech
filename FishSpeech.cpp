465273496e3245b7da6ac4516ee80e8bfaf4fb71
Pastamaster74 <aku.playstation@gmail.com>
2025-07-16 14:16:56 +0300

    haha talking fish

---
+++ FishSpeech.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << argv[1] << endl;
+    cout << "     \\" << endl;
+    cout << "     ________" << endl;
+    cout << "    / 0      \\/" << endl;
+    cout << "    \\________/\\" << endl;
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Fish Speech
+
+Make an ASCII fish say anything you want.
+
+Heavily inspired by cowsay.
+
+Very goofy and simple.
